d11a931039cc89ea72def57bec6c9a906121cc47
krisha-patel2006 <148178867+krisha-patel2006@users.noreply.github.com>
2023-10-19 12:10:21 +0530

    Add files via upload

---
+++ ext.js
@@ -0,0 +1,8 @@
+  
+function demo(){
+          if(confirm("Are you sure?")==true)
+          {
+          document.write("You have a pressed OK");}
+          else{document.write("Cancel pressed");
+        }
+    }
